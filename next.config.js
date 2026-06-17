ea9dd4d998924b8d52ea153f7ba36dd9b7731a9d
francescomariafalcone <francescofalconedesign@gmail.com>
2026-06-17 13:09:05 +0200

    Add next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   api: {
-    bodyParser: {
-      sizeLimit: '50mb',
-    },
+    bodyParser: false,
+    responseLimit: '50mb',
   },
 }
 
